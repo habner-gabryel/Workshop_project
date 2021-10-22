7fb7a61f702a69afb027280ecdd7bfbcbe7f0ed3
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-22 10:14:53 -0300

    Adicionado DepartmentForm

--- workshop_project1/src/gui/DepartmentListController.java
+++ workshop_project1/src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -37,8 +46,9 @@ public class DepartmentListController implements Initializable{
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtnNewAction() {
-		System.out.println("Créc ");
+	public void onBtnNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		crateDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	
@@ -68,6 +78,26 @@ public class DepartmentListController implements Initializable{
 		
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void crateDialogForm(String absolutName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			
+			dialogStage.setTitle("Enter department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}
+		catch (IOException e){
+			Alerts.showAlert("Deu ruim (IOException)", null, "Pois é Parceiro, " +e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
 }
